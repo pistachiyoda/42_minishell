c20b99dd0fc7b5a4cc7b60adf80497a5f3bb5323
mai <fujisawamai21@gmail.com>
2022-02-14 19:46:33 +0900

    "new"がc++では意味をもつキーワードでありコンパイルできないためこれを変更した。

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,21 +6,21 @@
 /*   By: fmai <fmai@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 03:46:19 by fmai              #+#    #+#             */
-/*   Updated: 2021/05/30 17:11:56 by fmai             ###   ########.fr       */
+/*   Updated: 2022/02/14 19:30:11 by fmai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_node)
 {
 	t_list	*old_last;
 
 	if (*lst == NULL)
-		*lst = new;
+		*lst = new_node;
 	else
 	{
 		old_last = ft_lstlast(*lst);
-		old_last->next = new;
+		old_last->next = new_node;
 	}
 }
