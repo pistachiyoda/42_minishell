78eb2f5cc2594c88549fbb87755b39b5c66eb125
増渕美和 <j.aimeparis93@gmail.com>
2022-02-16 03:17:51 +0900

    delimiterで分割する部分を関数分けた

--- sources/env/create_environ.c
+++ sources/env/create_environ.c
@@ -1,13 +1,13 @@
 #include "minishell.h"
 
-t_environ	*init_environ(void)
+t_environ	*init_environ(char *msg)
 {
 	t_environ	*env;
 
 	env = (t_environ *)malloc(sizeof(t_environ));
 	if (env == NULL)
 	{
-		print_error("malloc", EMESS_MALLOC_FAIL);
+		print_error(msg, EMESS_MALLOC_FAIL);
 		exit(EXIT_FAILURE);
 	}
 	env->key = NULL;
@@ -17,18 +17,30 @@ t_environ	*init_environ(void)
 	return (env);
 }
 
-t_environ	*add_environ(t_environ *env, t_environ *first_ele, char *env_str)
+char	**split_by_delimiter(char *env_str, char *msg)
+{
+	char		**split_ele;
+
+	split_ele = ft_split(env_str, '=');
+	if (split_ele == NULL)
+	{
+		print_error(msg, EMESS_MALLOC_FAIL);
+		exit(EXIT_FAILURE);
+	}
+	return (split_ele);
+}
+
+t_environ	*add_environ(t_environ *env, t_environ *first_ele,
+							char **split_ele, char *msg)
 {
 	t_environ	*new_ele;
-	char		**split_ele;
 
 	new_ele = (t_environ *)malloc(sizeof(t_environ));
 	if (new_ele == NULL)
 	{
-		print_error("malloc", EMESS_MALLOC_FAIL);
+		print_error(msg, EMESS_MALLOC_FAIL);
 		exit(EXIT_FAILURE);
 	}
-	split_ele = ft_split(env_str, '=');
 	new_ele->key = split_ele[0];
 	new_ele->value = split_ele[1];
 	new_ele->prev = env;
@@ -43,13 +55,16 @@ t_environ	*create_environ(char **envp)
 	int			i;
 	t_environ	*env;
 	t_environ	*first_ele;
+	char		*msg;
 
 	i = 0;
-	env = init_environ();
+	msg = "create_environ";
+	env = init_environ(msg);
 	first_ele = env;
 	while (envp[i] != NULL)
 	{
-		env = add_environ(env, first_ele, envp[i]);
+		env = add_environ(env, first_ele,
+				split_by_delimiter(envp[i], msg), msg);
 		i++;
 	}
 	return (env = env->next);
