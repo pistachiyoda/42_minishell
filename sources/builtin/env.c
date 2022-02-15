7f30cf1dfe98a930126bc7b944d52d7a27261d4c
増渕美和 <j.aimeparis93@gmail.com>
2022-02-15 17:32:45 +0900

    エラー処理の削除（parser側でまとめてエラー処理するため）

--- sources/builtin/env.c
+++ sources/builtin/env.c
@@ -2,11 +2,6 @@
 
 void	ft_env(t_cmd_block *cmd_block, t_environ *env)
 {
-	if (cmd_block->args[1] != NULL)
-	{
-		print_error(cmd_block->args[0], EMESS_INVALID_ARGS);
-		return ;
-	}
 	env = env->next;
 	while (env->key != NULL)
 	{
