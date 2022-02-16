a66839998926d45d3f72fcbfc12eff896efdc9f6
増渕美和 <j.aimeparis93@gmail.com>
2022-02-16 12:42:34 +0900

    builtin unset関数の追加

---
+++ sources/builtin/unset.c
@@ -0,0 +1,28 @@
+#include "minishell.h"
+
+void	ft_unset(t_cmd_block *cmd_block, t_environ *env)
+{
+	int			i;
+	int			len;
+	t_environ	*tmp;
+
+	i = 1;
+	while (cmd_block->args[i] != NULL)
+	{
+		env = env->next;
+		len = ft_strlen(cmd_block->args[i]);
+		while (env->key != NULL)
+		{
+			if (ft_strncmp(env->key, cmd_block->args[i], len) == 0)
+			{
+				env->prev->next = env->next;
+				env->next->prev = env->prev;
+				tmp = env;
+				env = env->prev;
+				free(tmp);
+			}
+			env = env->next;
+		}
+		i++;
+	}
+}
