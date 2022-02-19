c501ddd2729dd6cfc855c9f50e27b52f597687c7
増渕美和 <j.aimeparis93@gmail.com>
2022-02-19 21:59:13 +0900

    バグ３つの修正

--- sources/builtin/export.c
+++ sources/builtin/export.c
@@ -1,100 +1,44 @@
 #include "minishell.h"
 
-t_environ	*get_tmp_min(t_environ *env, int *min_i, char *flags, int f_len)
+bool	is_registered(t_environ *env, char **split_ele, bool key_only)
 {
-	int			i;
-	t_environ	*min;
+	size_t	len;
+	bool	registered;
 
-	i = 0;
-	min = env;
-	while (*min_i > i++)
-		min = min->next;
-	while (flags[*min_i] == '1')
-	{
-		min = min->next;
-		if (*min_i < f_len - 1)
-			(*min_i)++;
-		else
-		{
-			min = min->next->next;
-			*min_i = 1;
-		}
-	}
-	return (min);
-}
-
-void	print_statement(t_environ *min, int min_i, char *flags, int f_len)
-{
-	int			i;
-	t_environ	*head;
-
-	i = 0;
-	head = min;
-	if (min->value == NULL)
-		printf("declare -x %s\n", min->key);
-	else
-		printf("declare -x %s=\"%s\"\n", min->key, min->value);
-	while (head->key != NULL)
-		head = head->next;
-	while (i < f_len)
-	{
-		if (flags[i++] == '0')
-		{
-			display_sorted_env(head->next, min_i, flags, f_len);
-			break ;
-		}
-	}
-}
-
-void	display_sorted_env(t_environ *env, int min_i, char *flags, int f_len)
-{
-	int			i;
-	size_t		len;
-	t_environ	*min;
-
-	min = env;
-	min = get_tmp_min(env, &min_i, flags, f_len);
-	len = ft_strlen(min->key);
-	flags[min_i] = '1';
-	i = 0;
+	len = ft_strlen(split_ele[0]);
+	registered = false;
+	env = env->next;
 	while (env->key != NULL)
 	{
-		if ((ft_strncmp(min->key, env->key, len) > 0) && (flags[i] == '0'))
+		if (ft_strlen(env->key) > len)
+			len = ft_strlen(env->key);
+		if (ft_strncmp(env->key, split_ele[0], len) == 0)
 		{
-			flags[min_i] = '0';
-			min = env;
-			min_i = i;
-			len = ft_strlen(min->key);
-			flags[min_i] = '1';
+			if (!key_only)
+				env->value = split_ele[1];
+			registered = true;
 		}
 		env = env->next;
-		i++;
 	}
-	print_statement(min, min_i, flags, f_len);
+	return (registered);
 }
 
 int	update_environ(t_cmd_block *cmd_block, t_environ *env, int i)
 {
 	char	**split_ele;
-	bool	registered;
-	size_t	len;
+	bool	key_only;
 
-	registered = false;
+	key_only = false;
+	if (ft_strchr(cmd_block->args[i], '=') == NULL)
+		key_only = true;
 	split_ele = ft_split(cmd_block->args[i], '=');
-	printf("ele[0]: %s, ele[1]: %s\n", split_ele[0], split_ele[1]);
 	// malloc_check(split_ele, "export");
-	env = env->next;
-	len = ft_strlen(split_ele[0]);
-	while (env->key != NULL)
+	if (ft_isdigit(split_ele[0][0]) == 1)
 	{
-		if (ft_strncmp(env->key, split_ele[0], len) == 0)
-		{
-			env->value = split_ele[1];
-			registered = true;
-		}
-		env = env->next;
+		free_2d_array(split_ele);
+		return (-1);
 	}
-	if (!registered)
+	if (!is_registered(env, split_ele, key_only))
 	{
 		env = add_environ(env->prev, env, split_ele, "export");
 		env = env->next;
@@ -121,5 +65,12 @@ void	ft_export(t_cmd_block *cmd_block, t_environ *env)
 	}
 	i = 1;
 	while (cmd_block->args[i] != NULL)
+	{
 		i = update_environ(cmd_block, env, i);
+		if (i == -1)
+		{
+			print_error("export", EMESS_INVALID_ID);
+			return ;
+		}
+	}
 }
