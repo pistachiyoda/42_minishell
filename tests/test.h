ea108665a68f961775f03c0416e98cab942f4b83
mai <fujisawamai21@gmail.com>
2022-02-19 22:07:01 +0900

    テストサンプルコードの共有

--- tests/test.h
+++ tests/test.h
@@ -138,4 +138,23 @@ extern "C" {
 
 	// [後々削除]tmp_parser/tmp_parse_data.c 
 	void	tmp_parse_data(t_list **cmd_lst, char *str);
+
+	/////////////////////////////////////////////////////////////////////////////////////
+	// lexer.h
+	enum e_STATUS
+	{
+		QUOTE,
+		DQUOTE,
+		NONE
+	};
+
+	// lexer/add_last_str.c
+	bool	add_last_str(char *str, t_list *words, int i, int start);
+
+	// lexer/lexer.c
+	bool	is_space_tab_newline(char c);
+	int		is_in_quote_dquote(char *str, int i, int status);
+	int		split_by_space(char *str, t_list *words, int *i, int start);
+	int		split_by_redirect_pipe(char *str, t_list *words, int *i, int start);
+	bool	lexer(char *str);
 }
