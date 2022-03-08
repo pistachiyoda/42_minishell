0eee4547ead80dac3bb8a2359ea0bba2cc3f03c0
mai <fujisawamai21@gmail.com>
2022-03-08 11:21:14 +0900

    envpをt_environ_to_vectorで変換したものに差し替え

--- sources/main.c
+++ sources/main.c
@@ -8,6 +8,7 @@ int	main(int argc, char **argv, char **envp)
 	t_list		*words;
 	t_list		*cmd_list;
 	t_environ	*env;
+	char		**minishell_envp;
 
 	(void)argc;
 	(void)argv;
@@ -25,13 +26,12 @@ int	main(int argc, char **argv, char **envp)
 		print_cmd_lst(cmd_list);
 		if (is_fork_required(cmd_list))
 		{
-			g_status = exec_command_line(cmd_list, envp, ft_lstsize(cmd_list));
+			minishell_envp = t_environ_to_vector(env);
+			g_status = exec_command_line(cmd_list, minishell_envp, ft_lstsize(cmd_list));
 			printf("g_status = %d\n", g_status);
 		}
 		else
-		{
 			g_status = run_builtin_command(cmd_list->content, env);
-		}
 		free(str);
 	}
 }
