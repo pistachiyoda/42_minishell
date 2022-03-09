88858810f5b3b63dd785466f949b93625243d999
増渕美和 <j.aimeparis93@gmail.com>
2022-03-10 02:03:40 +0900

    メモリーリーク、ft_split関数の使用等の修正

--- sources/builtin/export.c
+++ sources/builtin/export.c
@@ -1,26 +1,21 @@
 #include "minishell.h"
 
-int	update_environ(t_cmd_block *cmd_block, t_environ *env, int i)
+void	update_environ(char *str, t_environ *env)
 {
 	char	**split_ele;
 	bool	key_only;
 
 	key_only = false;
-	if (ft_strchr(cmd_block->args[i], '=') == NULL)
-		key_only = true;
-	split_ele = ft_split(cmd_block->args[i], '=');
-	malloc_check(split_ele, "export");
-	if (ft_isdigit(split_ele[0][0]) == 1)
-	{
-		free_2d_array(split_ele);
-		return (-1);
-	}
-	if (is_env_registerd(env, split_ele, key_only) == NULL)
+	split_ele = split_by_delimiter(str, &key_only, "export");
+	if (is_valid_arg(split_ele[0])
+		&& is_env_registerd(env, split_ele, key_only, "export") == NULL)
 	{
 		env = add_environ(env->prev, env, split_ele, "export");
 		env = env->next;
+		free(split_ele);
 	}
-	return (++i);
+	else
+		free_2d_array(split_ele);
 }
 
 void	ft_export(t_cmd_block *cmd_block, t_environ *env)
@@ -29,12 +24,13 @@ void	ft_export(t_cmd_block *cmd_block, t_environ *env)
 	char	*flags;
 	int		env_num;
 
+	g_status = 0;
 	if (cmd_block->args[1] == NULL)
 	{
 		env_num = count_environ_ele(env);
 		flags = xmalloc(sizeof(char) * (env_num + 1), "export");
 		flags = ft_memset(flags, '0', env_num);
-		flags[env_num + 1] = '\0';
+		flags[env_num] = '\0';
 		display_sorted_env(env->next, 0, flags, env_num);
 		free(flags);
 		return ;
@@ -42,11 +38,7 @@ void	ft_export(t_cmd_block *cmd_block, t_environ *env)
 	i = 1;
 	while (cmd_block->args[i] != NULL)
 	{
-		i = update_environ(cmd_block, env, i);
-		if (i == -1)
-		{
-			print_error("export", EMESS_INVALID_ID);
-			return ;
-		}
+		update_environ(cmd_block->args[i], env);
+		i++;
 	}
 }
