420899d496349e07a1914e5b882a7b865f6cc967
増渕美和 <j.aimeparis93@gmail.com>
2022-03-17 19:53:23 +0900

    空白削除

--- sources/exec_command_line/handle_heredoc2.c
+++ sources/exec_command_line/handle_heredoc2.c
@@ -59,7 +59,7 @@ char	*expand_env_variables_in_buf(t_environ *env, char *buf)
 			joined_str = ft_xstrjoin_with_free(joined_str, value, "expand_env");
 		}
 		else
-		{		
+		{
 			tmp_str = ft_xmalloc(sizeof(char *) * 2, "expand_env");
 			tmp_str[0] = buf[i];
 			tmp_str[1] = '\0';
