042dc1c18ad5cb7a770325a633081134a3df35ca
増渕美和 <j.aimeparis93@gmail.com>
2022-03-18 17:17:37 +0900

    テスト追加

--- tests/builtin/export.cpp
+++ tests/builtin/export.cpp
@@ -1858,6 +1858,43 @@ TEST(export_G, has_double_quote15) {
 	compare_tokens(tokens, exp_tokens);
 }
 
+t_list	*has_invalid_envs(void)
+{
+	t_list		*exp_tokens;
+	t_cmd_block	*exp_cmd;
+	char		**exp_args;
+	t_redirects	*exp_redir;
+
+	exp_cmd = (t_cmd_block *)malloc(sizeof(t_cmd_block));
+	exp_redir = (t_redirects *)malloc(sizeof(t_redirects));
+	exp_redir->fd = 1;
+	exp_redir->redirect = WRITE;
+	exp_redir->target = ft_strdup("bb");
+	exp_redir->error = false;
+	exp_cmd->redirects = ft_lstnew(exp_redir);
+	exp_cmd->command = ft_strdup("echo");
+	exp_args = (char **)malloc(sizeof(char *) * 3);
+	exp_args[0] = ft_strdup("echo");
+	exp_args[1] = ft_strdup("bb");
+	exp_args[2] = NULL;
+	exp_cmd->args = exp_args;
+	exp_tokens = ft_lstnew(exp_cmd);
+	return (exp_tokens);
+}
+
+TEST(export_G, has_invalid_envs) {
+	t_list		*tokens;
+	t_list		*exp_tokens;
+	t_environ	*env;
+
+	env = create_environ(g_envp);
+	tokens = get_tokens_from_expansion(ft_strdup("export B=bb"), env);
+	ft_export((t_cmd_block *)tokens->content, env);
+	tokens = get_tokens_from_expansion(ft_strdup("  echo $BB$BB$B> \"$BB$BB$B\""), env);
+	exp_tokens = has_invalid_envs();
+	compare_tokens(tokens, exp_tokens);
+}
+
 t_list	*export_equal_signs_val(void)
 {
 	t_list		*exp_tokens;
