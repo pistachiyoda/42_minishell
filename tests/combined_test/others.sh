8a9adfd32726adf2cf20f3a4feefa5c6c435fa0c
mai <fujisawamai21@gmail.com>
2022-03-21 23:16:46 +0900

    ''や""が入力された場合のエラー処理

--- tests/combined_test/others.sh
+++ tests/combined_test/others.sh
@@ -2,3 +2,5 @@
 exec_test_error "unset PATH \n ls"
 exec_test_error "unset PATH \n echo hoge | ls | date"
 exec_test_error "unset PATH \n ls | date | echo hoge"
+exec_test_error "' '"
+exec_test_error "''"
