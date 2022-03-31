e27a37c11b4d4df2d987feea752bfb425a99e39f
mai <fujisawamai21@gmail.com>
2022-03-31 19:06:39 +0900

    テスト追加

--- tests/expand_env/expand_env_variables_in_buf.cpp
+++ tests/expand_env/expand_env_variables_in_buf.cpp
@@ -72,4 +72,152 @@ TEST(expand_env_G, multi_env_with_space)
 	STRCMP_EQUAL("/bin/zsh testmai test   ", ret);
 }
 
-// エッジケースっぽいの
+// single_quote
+TEST(expand_env_G, single_quote_after)
+{
+	char *ret;
+
+	ret = call_expand("$USER'");
+	STRCMP_EQUAL("mai'", ret);
+}
+
+TEST(expand_env_G, single_quote_before)
+{
+	char *ret;
+
+	ret = call_expand("'$USER");
+	STRCMP_EQUAL("'mai", ret);
+}
+
+TEST(expand_env_G, single_quoted_after_and_before)
+{
+	char *ret;
+
+	ret = call_expand("'$USER'");
+	STRCMP_EQUAL("'mai'", ret);
+}
+
+// double_quote
+TEST(expand_env_G, double_quote_after)
+{
+	char *ret;
+
+	ret = call_expand("$USER\"");
+	STRCMP_EQUAL("mai\"", ret);
+}
+
+TEST(expand_env_G, double_quote_before)
+{
+	char *ret;
+
+	ret = call_expand("\"$USER");
+	STRCMP_EQUAL("\"mai", ret);
+}
+
+TEST(expand_env_G, double_quoted_after_and_before)
+{
+	char *ret;
+
+	ret = call_expand("\"$USER\"");
+	STRCMP_EQUAL("\"mai\"", ret);
+}
+
+// +
+TEST(expand_env_G, plus_after)
+{
+	char *ret;
+
+	ret = call_expand("$USER+");
+	STRCMP_EQUAL("mai+", ret);
+}
+
+TEST(expand_env_G, plus_before)
+{
+	char *ret;
+
+	ret = call_expand("+$USER");
+	STRCMP_EQUAL("+mai", ret);
+}
+
+TEST(expand_env_G, plus_after_and_before)
+{
+	char *ret;
+
+	ret = call_expand("+$USER+");
+	STRCMP_EQUAL("+mai+", ret);
+}
+
+// -
+TEST(expand_env_G, minus_after)
+{
+	char *ret;
+
+	ret = call_expand("$USER-");
+	STRCMP_EQUAL("mai-", ret);
+}
+
+TEST(expand_env_G, minus_before)
+{
+	char *ret;
+
+	ret = call_expand("-$USER");
+	STRCMP_EQUAL("-mai", ret);
+}
+
+TEST(expand_env_G, minus_after_and_before)
+{
+	char *ret;
+
+	ret = call_expand("-$USER-");
+	STRCMP_EQUAL("-mai-", ret);
+}
+
+// _
+TEST(expand_env_G, underscore_after)
+{
+	char *ret;
+
+	ret = call_expand("$USER_");
+	STRCMP_EQUAL("", ret);
+}
+
+TEST(expand_env_G, underscore_before)
+{
+	char *ret;
+
+	ret = call_expand("_$USER");
+	STRCMP_EQUAL("_mai", ret);
+}
+
+TEST(expand_env_G, underscore_after_and_before)
+{
+	char *ret;
+
+	ret = call_expand("_$USER_");
+	STRCMP_EQUAL("_", ret);
+}
+
+// :
+TEST(expand_env_G, colon_after)
+{
+	char *ret;
+
+	ret = call_expand("$USER:");
+	STRCMP_EQUAL("mai:", ret);
+}
+
+TEST(expand_env_G, colon_before)
+{
+	char *ret;
+
+	ret = call_expand(":$USER");
+	STRCMP_EQUAL(":mai", ret);
+}
+
+TEST(expand_env_G, colon_after_and_before)
+{
+	char *ret;
+
+	ret = call_expand(":$USER:");
+	STRCMP_EQUAL(":mai:", ret);
+}
